3996b11ba001817c1a99fc4f365210bd14d0a5ac
pnguyn183 <binmaster0987@gmail.com~>
2025-10-25 05:03:25 +0700

    sửa config của auth

--- auth/src/config/index.js
+++ auth/src/config/index.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 
+let mongoURI = process.env.MONGODB_AUTH_URI;
+
+// Nếu đang chạy trong GitHub Actions (CI)
+if (process.env.NODE_ENV === "ci") {
+    mongoURI = "mongodb://mongodb:27017/authdb";
+}
+
 module.exports = {
-  mongoURI: process.env.MONGODB_AUTH_URI,
-  jwtSecret: process.env.JWT_SECRET || "secret",
+    mongoURI,
+    jwtSecret: process.env.JWT_SECRET || "secret",
 };
